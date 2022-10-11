5ff8976171a85d549d5f17eebfe91af73044335c
Matías Barrios <matias.barriosn@gmail.com>
2022-10-10 21:24:24 -0500

    fallback test deployment

--- scripts/fallback.ts
+++ scripts/fallback.ts
@@ -1,13 +1,30 @@
+import { expect } from "chai";
+import { generateKeyPair } from "crypto";
+import { parseEther } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 async function main() {
   const contractName = "Fallback";
-
-  const signer = await ethers.provider.getSigner();
-  const owner = signer.getAddress();
+  const { provider } = ethers;
+  const deployer = await ethers.provider.getSigner();
+  const attacker = await ethers.Wallet.createRandom();
+  // Fund attacker
+  await deployer.sendTransaction({
+    value: parseEther("10"),
+    to: attacker.address,
+  });
 
   const Contract = await ethers.getContractFactory(contractName);
   const contract = await Contract.deploy();
+
+  // contribute to contract
+  await contract.contribute({
+    value: parseEther("0.0009"),
+  });
+  let contractBalance = 
+  // trigger fallback
+  // check ownership
+  // check balance
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ test/Fallback.ts
@@ -0,0 +1,95 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+const contractName = "Fallback";
+
+describe(contractName, function () {
+  async function deployFixture() {
+    const [deployer, attacker] = await ethers.getSigners();
+    const { provider } = ethers;
+
+    const Contract = await ethers.getContractFactory(contractName);
+    const contract = await Contract.deploy();
+
+    return { contract, deployer, attacker, provider };
+  }
+
+  describe("Deployment", function () {
+    it("contract owner should be deployer", async function () {
+      const { contract, deployer } = await loadFixture(deployFixture);
+      const owner = await contract.owner();
+      expect(owner).to.be.eql(deployer.address);
+    });
+    it("contract balance should be 0", async function () {
+      const { contract, provider } = await loadFixture(deployFixture);
+      const contractBalance = await provider.getBalance(contract.address);
+      expect(contractBalance.toString()).to.be.eql("0");
+    });
+  });
+
+  // describe("Withdrawals", function () {
+  //   describe("Validations", function () {
+  //     it("Should revert with the right error if called too soon", async function () {
+  //       const { lock } = await loadFixture(deployFixture);
+
+  //       await expect(lock.withdraw()).to.be.revertedWith(
+  //         "You can't withdraw yet"
+  //       );
+  //     });
+
+  //     it("Should revert with the right error if called from another account", async function () {
+  //       const { lock, unlockTime, otherAccount } = await loadFixture(
+  //         deployFixture
+  //       );
+
+  //       // We can increase the time in Hardhat Network
+  //       await time.increaseTo(unlockTime);
+
+  //       // We use lock.connect() to send a transaction from another account
+  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
+  //         "You aren't the owner"
+  //       );
+  //     });
+
+  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
+  //       const { lock, unlockTime } = await loadFixture(deployFixture);
+
+  //       // Transactions are sent using the first signer by default
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw()).not.to.be.reverted;
+  //     });
+  //   });
+
+  //   describe("Events", function () {
+  //     it("Should emit an event on withdrawals", async function () {
+  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
+  //         deployFixture
+  //       );
+
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw())
+  //         .to.emit(lock, "Withdrawal")
+  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
+  //     });
+  //   });
+
+  //   describe("Transfers", function () {
+  //     it("Should transfer the funds to the owner", async function () {
+  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
+  //         deployFixture
+  //       );
+
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw()).to.changeEtherBalances(
+  //         [owner, lock],
+  //         [lockedAmount, -lockedAmount]
+  //       );
+  //     });
+  //   });
+  // });
+});
